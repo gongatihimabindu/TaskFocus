4b2a5fb4450dc5f3435c1b85d1a1931d78484bec
gongatihimabindu <47346992+gongatihimabindu@users.noreply.github.com>
2019-07-17 22:45:32 +0530

    Add files via upload

---
+++ TaskFocus/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import Hello from './Hello.js'
+//import Example from './Child.js'
+import Chips from './Chips.js'
+//import useStyles from './InputChip.js'
+class App extends React.Component{
+    
+    render(){
+        return(
+            <div className="App">
+                {/* <Hello/> */}
+                <Chips/>
+                {/* <useStyles/> */}
+                
+                
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ TaskFocus/src/index.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+
+ 
+
+import App from './App'
+
+// import './index.css'
+
+ 
+
+import ReactDOM from 'react-dom';
+
+ 
+
+ReactDOM.render(
+
+<App/>,
+
+document.getElementById('root')
+
+);
+
+
+
